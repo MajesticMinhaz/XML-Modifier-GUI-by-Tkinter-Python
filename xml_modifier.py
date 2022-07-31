f6b8f5e7468afecdb3a68752991c76b404f8f5ae
MD. MINHAZ <mdm047767@gmail.com>
2022-07-31 19:13:03 +0600

    Just jump to write functional code in xml_modifier.py file. Still now written code can open a empty window in screen, Nothing else.

---
+++ xml_modifier.py
@@ -0,0 +1,20 @@
+"""
+===========================================================================================================
+                                File        xml_modifier.py (Entry point)
+                                Author      Md. Minhaz
+                                Email       mdm047767@gmail.com
+                                Hire Me     https://pph.me/mdminhaz2003/
+                                Repo Link   https://github.com/mdminhaz2003/XML-Modifier-GUI-by-Tkinter-Python
+                                Location    Bangladesh
+                                Date        31-07-2022 at 16:01:00 (GMT + 6)
+===========================================================================================================
+"""
+from tkinter import Tk
+
+
+if __name__ == '__main__':
+    root = Tk()
+    root.title("XML Modifier")
+    root.resizable(width=False, height=False)
+
+    root.mainloop()
