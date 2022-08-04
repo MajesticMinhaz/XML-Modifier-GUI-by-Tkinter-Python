9099de5fad34cd5fabd451715d133f699daa9d38
MD. MINHAZ <mdm047767@gmail.com>
2022-08-04 23:04:59 +0600

    Added Two button read_files and submit button in app

--- ui.py
+++ ui.py
@@ -17,9 +17,25 @@ class Ui:
     def __init__(self, master: Widgets):
         self.master = master
 
+        self.read_files_btn = None
+        self.submit_btn = None
+
         self.app = {}
 
     def edit_text_packer(self) -> None:
         for key, value in zip(widgets_info.keys(), widgets_info.values()):
             if value.get("no_need_pack") is None:
                 self.app[key] = self.master.edit_text(manager=value)
+
+        self.read_files_btn = self.master.button(
+            btn_text="Read Files",
+            row=2,
+            column=2,
+            command=None
+        )
+        self.submit_btn = self.master.button(
+            btn_text="Submit",
+            row=49,
+            column=1,
+            command=None
+        )
