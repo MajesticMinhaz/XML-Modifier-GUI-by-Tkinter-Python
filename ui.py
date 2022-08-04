110ea75e3dcfb4420cab45e429efb410aa583205
MD. MINHAZ <mdm047767@gmail.com>
2022-08-04 22:29:34 +0600

    Added Edit Text widgets packer for loop in this file

--- ui.py
+++ ui.py
@@ -1,5 +1,14 @@
+from widgets_info import widgets_info
+from widgets import Widgets
+
 
 class Ui:
-    def __init__(self, master):
+    def __init__(self, master: Widgets):
         self.master = master
 
+        self.app = {}
+
+    def edit_text_packer(self) -> None:
+        for key, value in zip(widgets_info.keys(), widgets_info.values()):
+            if value.get("no_need_pack") is None:
+                self.app[key] = self.master.edit_text(manager=value)
